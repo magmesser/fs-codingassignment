26dabc6144ba372cf1025f364df8270022d4aae4
magmesser <magmesser@gmail.com>
2024-03-05 22:17:49 -0500

    Form and QC completed. Deploying and submitting.

---
+++ index.js
@@ -0,0 +1,17 @@
+function message() {
+    var name = document.getElementById('your-name');
+    var program = document.getElementById('program');
+    var check = document.getElementsByClassName('form-check-input');
+    const success = document.getElementById('success');    
+
+    if(name.value === '' || program.value === '') {
+        alert('Please complete all required fields to submit your  request.')
+    } else 
+    {
+        setTimeout(() => {
+            document.location.reload();
+        }, 5000)
+        success.style.display = 'block';
+        console.log("Form submitted successfully.")
+         };
+    } 
